8280f64dca71e2ab22888264969b030be87209b5
niko niko <niko@niko.io>
2019-11-19 17:59:45 +0100

    added helpful comment

--- src/mappers/definitions.js
+++ src/mappers/definitions.js
@@ -1,9 +1,13 @@
-
+// this file contains the forms of data used to identify what node is
+// they will be fed in to the z parrern matching library.
+// thought of as: "if a node has these elements in this format, then it is a [key]"
 
 const definitions = {
   definition: {
     tagName: 'define',
   },
+  // as an example: if a node has a tagname of 'element' and attr 'name'
+  // then we define it as a namedElement
   namedElement: {
     tagName: 'element',
     attributes: {
