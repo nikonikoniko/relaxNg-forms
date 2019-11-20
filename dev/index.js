a7868a479b72c0f7b3eeb9715ec4e69b27398243
niko niko <niko@niko.io>
2019-11-20 12:43:38 +0100

    automatic parsing

--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,7 @@
 const {parseRng} = require('../src/parsers');
 const {convert} = require('../src/createDom');
 
-console.log('goooodbyyyywwww');
+console.log('hello');
 
 const example = `
 
@@ -17,3 +17,21 @@ const example = `
 `
 
 const r = parseRng(convert(example));
+
+const schema = document.getElementById('schema');
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+console.log('oooooo')
+console.log(schema)
+
+schema.value = example;
+form.innerHTML = r;
+
+schema.onkeyup = function(e) {
+  const newschema = e.target.value;
+  console.log(e.target.value);
+  const r = parseRng(convert(newschema));
+  console.log(r);
+  form.innerHTML = r;
+};
